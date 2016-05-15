c775ccfc2c3ab52be2473199b9d0458a9418acb6
Adam Neumann <adam@noizwaves.com>
2016-05-15 11:14:34 -0600

    Rename app to Nile ;)

--- src/client/app/components/app.component.e2e.ts
+++ src/client/app/components/app.component.e2e.ts
@@ -1,19 +1,23 @@
 describe('App', () => {
 
-  beforeEach( () => {
-      browser.get('');
+  beforeEach(() => {
+    browser.get('');
   });
 
   it('should have a title', () => {
-      expect(browser.getTitle()).toEqual('My Angular2 App');
+    expect(browser.getTitle()).toEqual('Nile');
+  });
+
+  it('should have a heading', () => {
+    expect(element(by.css('sd-app sd-toolbar h1')).getText()).toEqual('Nile');
   });
 
   it('should have <nav>', () => {
-      expect(element(by.css('sd-app sd-navbar nav')).isPresent()).toEqual(true);
+    expect(element(by.css('sd-app sd-navbar nav')).isPresent()).toEqual(true);
   });
 
   it('should have correct nav text for Products', () => {
-      expect(element(by.css('sd-app sd-navbar nav a:first-child')).getText()).toEqual('PRODUCTS');
+    expect(element(by.css('sd-app sd-navbar nav a:first-child')).getText()).toEqual('PRODUCTS');
   });
 
 });
